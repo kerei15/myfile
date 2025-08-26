2ce0068445e9b3af9b6de030eda718f3482ff414
kerei <kereikhan2010@gmail.com>
2025-08-26 16:08:38 +0500

    operators

---
+++ Урок_30/operators.js
@@ -0,0 +1,46 @@
+// Задание 1
+let a = 10;
+let b = 5;
+let c = 2;
+let task1 = a + b + c;
+console.log(task1);
+let task2 = a + c - b;
+console.log(task2); 
+let task3 = a * b / c;
+console.log(task3);
+let task4 = a % b;
+console.log(task4);
+// Задание 2
+let x = 7;
+x++ + ++x;
+console.log(x);
+x = 7;
+x-- - --x;
+console.log(x);
+// Задание 3
+let text1 = "The result is:";
+let num1 = 4;
+let num2 = 5;
+let num3 = num1 + num2;
+let text2 = String(num3);
+let text3 = text1 + text2;
+console.log(text3);
+// Задание 4
+a = 10; 
+b = 5;
+c = 2;
+console.log(a > b && a > 0 && b > 0)
+console.log(c < 10 || c == 2)
+console.log(a / c != 5)
+// Задание 5
+let number1 = 10;
+let number2 = "10";
+console.log(number1 == number2); // Проверяет равенство значений без учета типа данных.
+console.log(number1 === number2); // Проверяет равенство значений с учетом типа данных.
+// Задание 6
+x = 9;
+console.log((x % 2 == 0 || x % 3 == 0) && x % 6 != 0)
+y = 8;
+console.log(y > 10 || y < 5)
+
+
